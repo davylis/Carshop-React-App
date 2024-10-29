768ef26b81e3b13f5c66c03bb4dfe98896ee8820
Liisa Davydov <liisamuttonen@gmail.com>
2024-10-29 10:56:27 +0200

    Add fetch functions

--- src/carapi.js
+++ src/carapi.js
@@ -16,3 +16,33 @@ export function deleteCar(url) {
         return response.json();
     })
 }
+
+export function saveCar(newCar) {
+    return fetch(import.meta.env.VITE_API_URL, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        //function that converts js object to a JSON
+        body: JSON.stringify(newCar)
+    })
+    .then(response =>{
+        if(!response.ok)
+            throw new Error("Error in saving: "+response.statusText);
+
+        return response.json();
+    })
+}
+
+export function updateCar(url, updateCar){
+    return fetch(url, {
+        method: "PUT",
+        headers: {"Content-type": "application/json"},
+        //function that converts js object to a JSON
+        body: JSON.stringify(updateCar)
+    })
+    .then(response =>{
+        if(!response.ok)
+            throw new Error("Error in update: "+response.statusText);
+
+        return response.json();
+    })
+}
